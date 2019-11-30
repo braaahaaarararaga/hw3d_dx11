745d67137168f51aacf459e898e4e6a3a71c9448
braaahaaarararaga <8576047@qq.com>
2019-11-30 15:20:22 +0900

    get rid off old vertexshader

--- Direct11EngineVS2017/Graphics/Shaders.cpp
+++ Direct11EngineVS2017/Graphics/Shaders.cpp
@@ -1,54 +1,5 @@
 ﻿#include "Shaders.h"
 
-bool VertexShader_::Initialize(Microsoft::WRL::ComPtr<ID3D11Device>& device, std::wstring shaderpath,
-	D3D11_INPUT_ELEMENT_DESC* layoutDesc, UINT numElements)
-{
-	HRESULT hr = D3DReadFileToBlob(shaderpath.c_str(), this->shader_buffer.GetAddressOf());
-
-	if (FAILED(hr))
-	{
-		std::wstring errorMsg = L"Failed to load shader: ";
-		errorMsg += shaderpath;
-		ErrorLogger::Log(hr, errorMsg);
-		return false;
-	}
-
-	hr = device->CreateVertexShader(this->shader_buffer->GetBufferPointer(), this->shader_buffer->GetBufferSize(),
-		NULL, this->shader.GetAddressOf());
-	if (FAILED(hr))
-	{
-		std::wstring errorMsg = L"Failed to create vertex shader: ";
-		errorMsg += shaderpath;
-		ErrorLogger::Log(hr, errorMsg);
-		return false;
-	}
-
-	hr = device->CreateInputLayout(layoutDesc, numElements, this->shader_buffer->GetBufferPointer(),
-		this->shader_buffer->GetBufferSize(), this->inputLayout.GetAddressOf());
-	if (FAILED(hr))
-	{
-		ErrorLogger::Log(hr, "FAILED TO CREATE INPUT LAYOUT");
-		return false;
-	}
-
-	return true;
-}
-
-ID3D11VertexShader * VertexShader_::GetShader()
-{
-	return this->shader.Get();
-}
-
-ID3D10Blob * VertexShader_::GetBuffer()
-{
-	return this->shader_buffer.Get();
-}
-
-ID3D11InputLayout * VertexShader_::GetInputLayout()
-{
-	return this->inputLayout.Get();
-}
-
 bool PixelShader_::Initialize(Microsoft::WRL::ComPtr<ID3D11Device>& device, std::wstring shaderpath)
 {
 	HRESULT hr = D3DReadFileToBlob(shaderpath.c_str(), this->shader_buffer.GetAddressOf());
--- Direct11EngineVS2017/Graphics/Shaders.h
+++ Direct11EngineVS2017/Graphics/Shaders.h
@@ -4,22 +4,6 @@
 #include <wrl/client.h>
 #include <d3dcompiler.h>
 
-
-
-class VertexShader_
-{
-public:
-	bool Initialize(Microsoft::WRL::ComPtr<ID3D11Device> &device, std::wstring shaderPath,
-		D3D11_INPUT_ELEMENT_DESC* layoutDesc, UINT numElements);
-	ID3D11VertexShader* GetShader();
-	ID3D10Blob* GetBuffer();
-	ID3D11InputLayout* GetInputLayout();
-private:
-	Microsoft::WRL::ComPtr<ID3D11VertexShader> shader;
-	Microsoft::WRL::ComPtr<ID3D10Blob> shader_buffer;
-	Microsoft::WRL::ComPtr<ID3D11InputLayout> inputLayout;
-};
-
 class PixelShader_
 {
 public:
